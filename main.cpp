7f20cd6f3b38a3999343c3d77228c69e20f638e3
MadalinaJitaru <jitaru_mada@yahoo.com>
2017-01-13 22:11:08 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 //#include"Header.h";
-
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -7,13 +6,22 @@
 #include<cstdlib>
 #include<time.h>
 #include <stdio.h>
+#include <windows.h>
 using namespace std;
 #define MAX_LINII 9
 #define MAX_COLOANE 9
+
 struct fill_empty
 {
 	bool ok;
 }matrix1[MAX_LINII][MAX_COLOANE];
+
+void setColor(unsigned short color)
+{
+    HANDLE hcon;
+    hcon = GetStdHandle (STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute (hcon ,color);
+}
 /*bool fileExists(const std::string& filename)
 {
 struct stat buf;
@@ -36,6 +44,7 @@ writefile<<'\n\<<numeUtilizator;
 ofstream write(numeUtilizator.c_str());
 
 }*/
+
 void write(int matrix[MAX_LINII][MAX_COLOANE])
 {
 	ofstream write("WriteEasy.txt");
@@ -75,8 +84,18 @@ void write(int matrix[MAX_LINII][MAX_COLOANE])
 				cout << "|" << " ";
 				write << "|" << " ";
 			}
-			cout << matrix[line][column] << " ";
-			write << matrix[line][column] << " ";
+			if(matrix1[line][column].ok == 0)
+            {
+                setColor(1);
+                cout << matrix[line][column] << " ";
+                write << matrix[line][column] << " ";
+                setColor(7);
+            }
+            else
+            {
+                cout << matrix[line][column] << " ";
+                write << matrix[line][column] << " ";
+            }
 		}
 		cout << "|";
 		write << "|";
@@ -100,46 +119,97 @@ void write(int matrix[MAX_LINII][MAX_COLOANE])
     cout << endl;
 }
 
-void addValue(int matrix[][MAX_COLOANE], int line, int column, int value)
+void addValue(int matrix[][MAX_COLOANE], int line, int column, int value, int &spaceFree)
 {
 
-	if (matrix1[line][column].ok == false)
+	if (matrix1[line][column].ok == false && matrix[line][column] == 0)
 	{
 		matrix[line][column] = value;
+		spaceFree--;
 	}
 
 }
 
-void Elimin(int matrix[][MAX_COLOANE], int line, int column)
+void Elimin(int matrix[][MAX_COLOANE], int line, int column, int &spaceFree)
 {
-	if (matrix1[line][column].ok == false)
+	if (matrix1[line][column].ok == false && matrix[line][column] != 0)
 	{
 		matrix[line][column] = 0;
+		spaceFree++;
 	}
 	else
     {
-        cout << "You choose the wrong celule";
+        cout << "You choose the wrong celule" << endl;
+    }
+}
+void readEasySudoku(int matrix[][MAX_COLOANE],int correct[][MAX_COLOANE])
+{
+
+    ifstream f1("Easy.txt");
+    for (int line = 0; line < 9; line++)
+    {
+        for (int column = 0; column < 9; column++)
+        {
+            f1 >> matrix[line][column];
+            correct[line][column] = matrix[line][column];
+            matrix1[line][column].ok = 1;
+        }
+    }
+    f1.close();
+}
+void readMediumSudoku(int matrix[][MAX_COLOANE],int correct[][MAX_COLOANE])
+{
+
+    ifstream f2("Medium.txt");
+    for (int line = 0; line < 9; line++)
+    {
+        for (int column = 0; column < 9; column++)
+        {
+            f2 >> matrix[line][column];
+            correct[line][column] = matrix[line][column];
+            matrix1[line][column].ok = 1;
+        }
+    }
+    f2.close();
+}
+void readHardSudoku(int matrix[][MAX_COLOANE],int correct[][MAX_COLOANE])
+{
+
+    ifstream f3("Hard.txt");
+    for (int line = 0; line < 9; line++)
+    {
+        for (int column = 0; column < 9; column++)
+        {
+            f3 >> matrix[line][column];
+            correct[line][column] = matrix[line][column];
+            matrix1[line][column].ok = 1;
+        }
+    }
+    f3.close();
+}
+void readSudoku(int matrix[][MAX_COLOANE],int correct[][MAX_COLOANE],int nivel)
+{
+    if(nivel == 1)
+    {
+        readEasySudoku(matrix,correct);
+    }
+    else
+    {
+        if(nivel == 2)
+        {
+            readMediumSudoku(matrix,correct);
+        }
+        else
+        {
+            if(nivel == 3)
+            {
+                readHardSudoku(matrix,correct);
+            }
+        }
     }
 }
 
 
-void readEasySudoku(int matrix[][MAX_COLOANE],int easy[][MAX_COLOANE])
-{
-	ifstream f1("Easy.txt");
-	//ifstream f1copy("Easycorrect.txt");
-	for (int line = 0; line < 9; line++)
-	{
-		for (int column = 0; column < 9; column++)
-		{
-			f1 >> matrix[line][column];
-			easy[line][column] = matrix[line][column];
-			matrix1[line][column].ok = 1;
-		}
-	}
-	f1.close();
-	//f1copy.close();
-
-}
 void random (int matrix[][MAX_COLOANE],int spaceFree)
 {
     int line, column, index = 0;
@@ -210,13 +280,13 @@ bool valid(int line, int column, int value, int matrix[MAX_LINII][MAX_COLOANE])
 	}
 }
 
-bool checkFinal(int matrix[MAX_LINII][MAX_COLOANE], int easy[MAX_LINII][MAX_COLOANE])
+bool checkFinal(int matrix[MAX_LINII][MAX_COLOANE], int correct[MAX_LINII][MAX_COLOANE])
 {
 	for (int line = 0; line < 9; line++)
 	{
 		for (int column = 0; column < 9; column++)
 		{
-			if (matrix[line][column] == 0 || matrix[line][column] != easy[line][column])
+			if (matrix[line][column] == 0 || matrix[line][column] != correct[line][column])
 				return false;
 		}
 	}
@@ -226,7 +296,7 @@ void click_ADD (int number,int matrix[][MAX_COLOANE],int &spaceFree)
 {
     int line, column, value, number1;
     int correct = 0;
-    while (correct == 0 ||  spaceFree!= 0)
+    while (correct == 0 &&  spaceFree!= 0)
     {
         cout << "Enter the values that you need in order : line , column and value for sudoku " << endl;
         cin >> line >> column >> value;
@@ -239,22 +309,26 @@ void click_ADD (int number,int matrix[][MAX_COLOANE],int &spaceFree)
             {
                 if(valid(line-1,column-1,value,matrix))
                 {
+                    setColor(5);
                     cout <<"GOOD JOB" << endl;
+                    setColor(7);
                 }
                 else
                 {
+                    setColor(4);
                     cout<<"NO, the number is not correct. You have to elimine the number put in sudoku :D" << endl;
+                    setColor(7);
                 }
             }
-
-            spaceFree--;
-            addValue(matrix, line-1, column-1, value);
+            addValue(matrix, line-1, column-1, value,spaceFree);
             write(matrix);
             correct = 1;
           }
           else
           {
+              setColor(4);
               cout << "Enter numbers from 1 to 9";
+              setColor(7);
           }
     }
 }
@@ -269,144 +343,149 @@ void click_ELIMINE (int number,int matrix[][MAX_COLOANE],int &spaceFree)
         cin >> line >> column;
         if (line >= 1 && line <= 9 && column >= 1 && column <= 9)
         {
-            Elimin(matrix, line-1, column-1);
-            spaceFree++;
+            Elimin(matrix, line-1, column-1,spaceFree);
             write(matrix);
             correct = 1;
         }
         else
         {
+            setColor(4);
             cout << "Enter numbers from 1 to 9";
+            setColor(7);
         }
     }
 }
-void easySudoku(int matrix[][MAX_COLOANE], int easy[][MAX_COLOANE],int &spaceFree)
+void solveSudoku(int matrix[][MAX_COLOANE], int correct[][MAX_COLOANE],int &spaceFree,int nivel)
 {
-	easySudoku:readEasySudoku(matrix,easy);
-	write(matrix);
-	random(matrix,10);
-	write(matrix);
-	int number, number1 , line, column, value;
-	while (!checkFinal(matrix,easy))
-	{
-		int correct1 = 0;
-		if (correct1 == 0)
-		{
-			cout << "Enter < 1 > ADD a value" << "< 2 > Elimine on value " << "< 3 > Replay " << "< 4 > HELP " << endl;
-			cin >> number;
-			if (number == 1)
-			{
-				click_ADD(number,matrix,spaceFree);
-			}
-			else
-			{
-				if (number == 2)
-				{
-					click_ELIMINE(number,matrix,spaceFree);
-				}
-				else
-				{
-					if (number == 3)
-					{
-						readEasySudoku(matrix,easy);
-						write(matrix);
-					}
-					else
-					{
-						if (number == 4)
-						{
-							cout << "You have to choose the number that doesn't exist on the same line , column or in the same square that you choose them.Try to choose the number which it showes the most of the time :) ! " << endl;
-						}
-						else
-						{
-							cout << "Enter the wrong number , please try again..." << endl;
-							correct1 = 0;
-						}
-					}
-				}
-			}
-			correct1 = 1;
-		}
+	solveSudoku:
+	    readSudoku(matrix,correct,nivel);
+        write(matrix);
+        random(matrix,spaceFree+1);
+        write(matrix);
+        int number, number1 , line, column, value;
+        while (!checkFinal(matrix,correct))
+        {
+         _begin:cout << "Enter < 1 > ADD a value" << "< 2 > Elimine on value " << "< 3 > Replay " << "< 4 > HELP " << endl;
+                cin >> number;
+                if (number == 1)
+                {
+                    click_ADD(number,matrix,spaceFree);
+                }
+                else
+                {
+                    if (number == 2)
+                    {
+                        click_ELIMINE(number,matrix,spaceFree);
+                    }
+                    else
+                    {
+                        if (number == 3)
+                        {
+                            readSudoku(matrix,correct,nivel);
+                            write(matrix);
+                        }
+                        else
+                        {
+                            if (number == 4)
+                            {
+                                setColor(6);
+                                cout << "You have to choose the number that doesn't exist on the same line , column or in the same square that you choose them.Try to choose the number which it showes the most of the time :) ! " << endl;
+                                setColor(7);
+                            }
+                            else
+                            {
+                                setColor(4);
+                                cout << "Enter the wrong number , please try again..." << endl;
+                                setColor(7);
+                                goto _begin;
+                            }
+                        }
+                    }
+                }
 
-	}
-    cout << "Congration!" << endl << "If you want to play more , enter < 1 >";
-    int number2;
-    cin >> number2;
-    if(number2 == 1)
-    {
-        goto easySudoku;
-    }
-
-
+        }
+        setColor(5);
+        cout << "Congration!" << endl;
+        setColor(7);
+        cout << "If you want to play more , enter < 1 >";
+        int number2;
+        cin >> number2;
+        if(number2 == 1)
+        {
+            goto solveSudoku;
+        }
 }
-void read_menu(int matrix[][MAX_COLOANE],int easy[][MAX_COLOANE],int spaceFree)
+void read_menu(int matrix[][MAX_COLOANE],int correct[][MAX_COLOANE],int spaceFree)
 {
-	cout << "Enter < 1 > to begin the game" << endl << "< 2 > for more info..." << endl;
-	int number;
-	cin >> number;
-	if (number == 1)
-	{
-		cout << "Enter < 1 > for easy sudoku" << endl << "< 2 > for medium sudoku " << endl << "< 3 > for hard sudoku" << endl << "< 4 > back to menu" << endl;
-		int number;
-		cin >> number;
-		if (number == 1)
-		{
-			easySudoku(matrix,easy,spaceFree);
-		}
-		/*else
-		{
-			if (number == 2)
-			{
-				mediumSudoku(matrix);
-			}
-			else
-			{
-				if (number == 3)
-				{
-					hardSudoku(matrix);
-				}
-				else
-				{
-					if (number == 4)
-					{
-						read_menu(matrix);
-					}
-					else
-					{
-						cout << "You enter a wrong number , please try again...";
-						read_menu(matrix);
-					}
-				}
-			}
-		}
-	}
-	else
-	{
-		if (number == 2)
-		{
-			cout << "Number puzzles appered in newspapers in the late 19th century, when French puzzl setters began experimenting with removing numbers from magic squares.It was not a Sudoku because it contained double-digit numbers and require aritmetic rather than logic to solve , but it shared key characteristics: each row, column and sub-square added up to the same number. " << endl;
-			cout << "In this game you will choose square by Enter the number of row and space and the number of column . Then you will add a number from 1 to 9." << endl;
-			read_menu(matrix);
-		}
-		else
-		{
-			cout << "You enter a wrog nmber, please try again...";
-			read_menu(matrix);
-		}*/
-	}
-
+    read_menu:
+        cout << "Enter < 1 > to begin the game" << endl << "< 2 > for more info..." << endl;
+        int number;
+        cin >> number;
+        if (number == 1)
+        {
+            cout << "Enter < 1 > for easy sudoku" << endl << "< 2 > for medium sudoku " << endl << "< 3 > for hard sudoku" << endl << "< 4 > back to menu" << endl;
+            int number;
+            cin >> number;
+            if (number == 1)
+            {
+                spaceFree = 17;
+                solveSudoku(matrix,correct,spaceFree,1);
+            }
+            else
+            {
+                if (number == 2)
+                {
+                    spaceFree = 32;
+                    solveSudoku(matrix,correct,spaceFree,2);
+                }
+                else
+                {
+                    if (number == 3)
+                    {
+                        spaceFree = 50;
+                        solveSudoku(matrix,correct,spaceFree,3);
+                    }
+                    else
+                    {
+                        if (number == 4)
+                        {
+                            goto read_menu;
+                        }
+                        else
+                        {
+                            setColor(4);
+                            cout << "You enter a wrong number , please try again..." << endl;
+                            setColor(7);
+                            goto read_menu;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (number == 2)
+            {
+                setColor(3);
+                cout << "Number puzzles appered in newspapers in the late 19th century, when French puzzl setters began experimenting with removing numbers from magic squares.It was not a Sudoku because it contained double-digit numbers and require aritmetic rather than logic to solve , but it shared key characteristics: each row, column and sub-square added up to the same number. " << endl;
+                cout << "In this game you will choose square by Enter the number of row and space and the number of column . Then you will add a number from 1 to 9." << endl;
+                setColor(7);
+                goto read_menu;
+            }
+            else
+            {
+                setColor(4);
+                cout << "You enter a wrog nmber, please try again..." << endl;
+                setColor(7);
+                goto read_menu;
+            }
+        }
 }
-
-
-
-
-
-
 int main()
 {
-	int matrix[MAX_LINII][MAX_COLOANE], easy[MAX_LINII][MAX_COLOANE];
+	int matrix[MAX_LINII][MAX_COLOANE], correct[MAX_LINII][MAX_COLOANE];
 	int spaceFree;
-	read_menu(matrix, easy,spaceFree);
+	read_menu(matrix, correct,spaceFree);
 
 	return 0;
 
